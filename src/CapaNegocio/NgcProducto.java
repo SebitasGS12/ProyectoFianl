64c006c0d7ac16fe71d4f03095f909d6329e6051
LENOVO <LENOVO@SebastianGS>
2022-11-25 15:49:42 -0500

    edit

--- src/CapaNegocio/NgcProducto.java
+++ src/CapaNegocio/NgcProducto.java
@@ -1,5 +1,42 @@
 package CapaNegocio;
 
-public class NgcProducto {
+
+import java.util.ArrayList;
+
+
+import Clases.Producto;
+import Datos.clsConexionProducto;
+
+
+public class NgcProducto{
+
+	private clsConexionProducto ObjProducto;
+	
+	public NgcProducto() {
+		ObjProducto = new clsConexionProducto();
+	}
+	
+	public ArrayList<Producto> Lista() {
+		return ObjProducto.ListarProducto();
+	}
+	
+	public Producto Buscar(int id){
+		return ObjProducto.BuscarProducto(id);
+	}
+	
+	public void Insertar(Producto ObjP) {
+		ObjProducto.InsertarProducto(ObjP);
+	}
+	
+	public void Modificar(Producto ObjP) {
+		ObjProducto.ModificarProducto(ObjP);
+	}
+	
+	public void Eliminar(int cod) {
+		ObjProducto.EliminarProducto(cod);
+	}
+	
+	
+	
 
 }
